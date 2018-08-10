6ce4af17ee971ce81ab6738f7a410d4224e99b6b
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-08-10 12:54:42 +0200

    Implemented POST request to accounts

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Account = require('../models/account.model.js');
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
-  
+  console.log(req.body);
+  Account.findOne({ email }, (err, account) => {
+    if (err) {
+      console.log('Error in finding account ' + err);
+      res.send('Error creating accout');
+      return;
+    }
+
+    if (account) {
+      res.status(405).send('Error creating accout');
+      return;
+    }
+
+    const newAccount = new Account({ email: email, password: password });
+    newAccount.save(err => {
+      if (err) {
+        console.log('Error saving new account ' + err);
+        res.send('Error creating accout');
+        return;
+      }
+
+      res.status(201).send(newAccount);
+    });
+  });
 });
 
 module.exports = router;
-
-/*
- {
-   email: ...
-   password: ....
- }
-*/
